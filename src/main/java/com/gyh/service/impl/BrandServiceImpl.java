485d8e82e8e3c10d81d3a96c92f06f15f1e10802
Seacolour <1326192454@qq.com>
2023-10-28 17:17:32 +0800

    Initial commit

--- src/main/java/com/gyh/service/impl/BrandServiceImpl.java
+++ src/main/java/com/gyh/service/impl/BrandServiceImpl.java
@@ -13,23 +13,18 @@ public class BrandServiceImpl implements BrandService {
     //1. 创建SqlSessionFactory 工厂对象
     SqlSessionFactory factory = SqlSessionFactoryUtils.getSqlSessionFactory();
 
-
     @Override
     public List<Brand> selectAll() {
         //2. 获取SqlSession对象
         SqlSession sqlSession = factory.openSession();
         //3. 获取BrandMapper
         BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
-
         //4. 调用方法
         List<Brand> brands = mapper.selectAll();
-
         //5. 释放资源
         sqlSession.close();
-
         return brands;
     }
-
     @Override
     public void add(Brand brand) {
         //2. 获取SqlSession对象
@@ -41,6 +36,30 @@ public class BrandServiceImpl implements BrandService {
         mapper.add(brand);
         sqlSession.commit();//提交事务
 
+        //5. 释放资源
+        sqlSession.close();
+    }
+    @Override
+    public void update(Brand brand) {
+        //2. 获取SqlSession对象
+        SqlSession sqlSession = factory.openSession();
+        //3. 获取BrandMapper
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+        //4. 调用方法
+        mapper.update(brand);
+        sqlSession.commit();//提交事务
+        //5. 释放资源
+        sqlSession.close();
+    }
+    @Override
+    public void delete(int id) {
+        //2. 获取SqlSession对象
+        SqlSession sqlSession = factory.openSession();
+        //3. 获取BrandMapper
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+        //4. 调用方法
+        mapper.delete(id);
+        sqlSession.commit();//提交事务
         //5. 释放资源
         sqlSession.close();
     }
