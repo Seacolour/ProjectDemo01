c706d3a64300f5a66fbf8ad40f5a50e10df2de05
Seacolour <1326192454@qq.com>
2023-11-01 16:41:43 +0800

    Initial commit

--- src/main/java/com/gyh/service/BrandService.java
+++ src/main/java/com/gyh/service/BrandService.java
@@ -8,23 +8,27 @@ public interface BrandService {
 
     /**
      * 查询所有
+     *
      * @return
      */
     List<Brand> selectAll();
 
     /**
      * 添加数据
+     *
      * @param brand
      */
     void add(Brand brand);
 
     /*
-    *更新数据
-    * */
+     *更新数据
+     * */
     void update(Brand brand);
 
     /*
-    *删除数据
-    * */
+     *删除数据
+     * */
     void delete(int id);
+
+    void deleteByIds(int[] ids);
 }
